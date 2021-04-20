d20a40b9ac5da532a7e2d7a368fc84b1f42cc0a7
Fatih <fatihayoz20800@gmail.com>
2021-04-21 00:24:44 +0200

    isolate exercises

--- isolate/23-local-param-parent/examples/0-the-question.js
+++ isolate/23-local-param-parent/examples/0-the-question.js
@@ -27,21 +27,21 @@ const findTheType = param => {
 
 console.log('-- example 1 --');
 const arg1 = 5;
-const returned1 = findTheType(arg1);
+const returned1 = findTheType(arg1); // typeof return string
 console.log(typeof returned1, returned1);
-// console.log(typeof param, param);
-// console.log(typeof result, result);
+// console.log(typeof param, param); // param is not defined outside the function
+// console.log(typeof result, result); // result is not defined outside the function
 
 console.log('-- example 2 --');
 const arg2 = 'hi!';
-const returned2 = findTheType(arg2);
+const returned2 = findTheType(arg2); 
 console.log(typeof returned2, returned2);
-// console.log(typeof param, param);
-// console.log(typeof result, result);
+// console.log(typeof param, param); // param is not defined outside the function
+// console.log(typeof result, result); // result is not defined outside the function
 
 console.log('-- example 3 --');
 const arg3 = null;
 const returned3 = findTheType(arg3);
 console.log(typeof returned3, returned3);
-// console.log(typeof param, param);
-// console.log(typeof result, result);
+// console.log(typeof param, param); // param is not defined outside the function
+// console.log(typeof result, result); // result is not defined outside the function
